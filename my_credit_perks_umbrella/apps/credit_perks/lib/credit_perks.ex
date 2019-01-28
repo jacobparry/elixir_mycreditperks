443279721a7f5693e7959f48e225b7c267fd2f0e
Jacob Parry <jacob.parry@divvypay.com>
2019-01-28 09:58:10 -0700

    1.1-setup-elixir-logic-app

---
+++ my_credit_perks_umbrella/apps/credit_perks/lib/credit_perks.ex
@@ -0,0 +1,18 @@
+defmodule CreditPerks do
+  @moduledoc """
+  Documentation for CreditPerks.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CreditPerks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
