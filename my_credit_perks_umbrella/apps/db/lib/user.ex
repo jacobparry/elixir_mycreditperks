e05b78f9ee592b75570f04c3057e0aada94026f9
Jacob Parry <jacob.parry@divvypay.com>
2019-01-28 12:22:37 -0700

    2.0-ecto-models-user

---
+++ my_credit_perks_umbrella/apps/db/lib/user.ex
@@ -0,0 +1,24 @@
+defmodule Db.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "users" do
+    ###### 2.0-ecto-models-user
+    field(:username, :string)
+    field(:password, :string)
+    field(:email, :string)
+    field(:age, :integer)
+    timestamps()
+    ###################
+  end
+
+  @required_fields [:username, :password, :email]
+  @optional_fields [:age]
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
