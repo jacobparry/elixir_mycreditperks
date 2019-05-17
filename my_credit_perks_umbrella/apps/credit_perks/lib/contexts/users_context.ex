ce22ffeae7cabab1383be37e536d1f8e1930f557
Jacob Parry <jacob.parry@divvypay.com>
2019-05-17 11:54:05 -0600

    5.7-fixing-resolver-contexts

--- my_credit_perks_umbrella/apps/credit_perks/lib/contexts/users_context.ex
+++ my_credit_perks_umbrella/apps/credit_perks/lib/contexts/users_context.ex
@@ -3,7 +3,8 @@ defmodule CreditPerks.Contexts.UsersContext do
   alias Db.Repo
 
   alias Db.Models.{
-    User
+    User,
+    UserCard
   }
 
   def get_all do
@@ -46,5 +47,63 @@ defmodule CreditPerks.Contexts.UsersContext do
 
   def delete(user) do
     Repo.delete(user)
+  end
+
+  def find_all_users(%{matching: matching} = _params) do
+    query =
+      from(u in User,
+        where: ilike(u.username, ^"%#{matching}%")
+      )
+
+    {:ok, Repo.all(query)}
+  end
+
+  def find_all_users(%{order: order} = _params) do
+    query =
+      from(u in User,
+        order_by: {^order, u.username}
+      )
+
+    {:ok, Repo.all(query)}
+  end
+
+  def find_all_users(_params) do
+    {:ok, Repo.all(User)}
+  end
+
+  def find_all_users_with_filters(%{filter: filters} = _params) do
+    query =
+      from(u in User)
+      |> matching_filter(filters[:matching])
+      |> order_filter(filters[:order])
+
+    {:ok, Repo.all(query)}
+  end
+
+  def matching_filter(query, nil), do: query
+
+  def matching_filter(query, matching_string) do
+    from(u in query,
+      where: ilike(u.username, ^"%#{matching_string}%")
+    )
+  end
+
+  def order_filter(query, nil), do: query
+
+  def order_filter(query, order) do
+    from(u in query,
+      order_by: {^order, u.username}
+    )
+  end
+
+  def find_cards_for_user(%User{} = user) do
+    query =
+      from(uc in UserCard,
+        join: c in assoc(uc, :card),
+        where: uc.user_id == ^user.id,
+        select: c
+      )
+
+    {:ok, Repo.all(query)}
   end
 end
